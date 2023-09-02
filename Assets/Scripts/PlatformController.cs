1c80ae81d00e60c5615e91bd58aa98b2ce86c2cf
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-09-02 22:41:08 +0200

    Add parkour pillars

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public bool isUp = true;
+    
+    [SerializeField] private float moveDownBy;
+    private float _upHeight;
+    
+    private void Awake()
+    {
+        _upHeight = transform.position.y;
+    }
+
+    [SuppressMessage("ReSharper", "Unity.InefficientPropertyAccess")]
+    private void OnDrawGizmos()
+    {
+        Vector3 scale = transform.localScale * 1.01f; // Removes z-fighting on the sides
+        scale.y = transform.localScale.y;
+        
+        Gizmos.color = Color.grey;
+        Gizmos.DrawCube(transform.position + Vector3.down * moveDownBy, scale);
+    }
+}
